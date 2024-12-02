e12bb85f0d9ff6206beeeff93163b8694640ff0f
Johan Gustafsson <johan.gustafsson@bejoynd.com>
2024-12-02 13:12:51 +0100

    day2

--- bin/adventofcode.dart
+++ bin/adventofcode.dart
@@ -31,10 +31,48 @@ void day1(List<String> input) {
   print(similarity);
 }
 
+void day2(List<String> input) {
+  bool areLevelsSafe(List<int> levels) {
+    var increasing = levels[1] > levels[0];
+    var isAllSafe = true;
+    for (var i = 1; i < levels.length; i++) {
+      final diff = levels[i] - levels[i - 1];
+      final diffAbs = diff.abs();
+      isAllSafe &= (diffAbs >= 1 && diffAbs <= 3) && ((increasing && diff > 0) || (!increasing && diff < 0));
+    }
+
+    return isAllSafe;
+  }
+
+  var safeReports = 0;
+  var safeIfProblemDampenerIsEnabled = 0;
+  for (final line in input) {
+    final levels = line.split(' ').map(int.parse).toList();
+
+    if (areLevelsSafe(levels)) {
+      safeReports++;
+    }
+
+    // Validate with the problem dampener enabled!
+    for (var i = 0; i < levels.length; i++) {
+      var levelsWithOneRemoved = List<int>.from(levels);
+      levelsWithOneRemoved.removeAt(i);
+      if (areLevelsSafe(levelsWithOneRemoved)) {
+        safeIfProblemDampenerIsEnabled++;
+        break;
+      }
+    }
+  }
+
+  print(safeReports);
+  print(safeIfProblemDampenerIsEnabled);
+}
+
 void main(List<String> arguments) {
-  const defaultDay = 'day1';
-  final day = arguments.length > 0 ? arguments[0] : defaultDay;
-  final inputFilePath = 'bin/${day}.txt';
+  const defaultDay = 'day2';
+  final useSampleInput = arguments.length > 1 ? arguments[1] == '--sample' : false;
+  final day = arguments.isNotEmpty ? arguments[0] : defaultDay;
+  final inputFilePath = 'bin/$day${useSampleInput ? '_sample' : ''}.txt';
 
   final inputFile = File(inputFilePath);
   final inputLines = inputFile.readAsLinesSync();
@@ -42,6 +80,9 @@ void main(List<String> arguments) {
   switch (day) {
     case 'day1':
       day1(inputLines);
+    break;
+    case 'day2':
+      day2(inputLines);
     break;
   }
 }
